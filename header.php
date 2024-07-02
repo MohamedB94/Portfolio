e1e47aa914802b1d0c25897eefaba87079b42d94
mbenasr <mbenasr@insta.fr>
2024-07-02 16:33:11 +0200

    debut de la page projet

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
         <li><a href="index.php">Bienvenue Sur Mon Portfolio !</a></li>
         <li><a href="cv.php">MON CV</a></li>
         <li><a href="veille.php">VEILLE TECNOLOGIQUE</a></li>
-        <li><a href="#projets">PROJETS</a></li>
+        <li><a href="projet.php">PROJETS</a></li>
         <li><a href="#stages">STAGES</a></li>
         <li><a href="#competences">FICHE DE MES COMPÉTENCES</a></li>
     </ul>
---
+++ projet.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="projet.css">
+    <title>Mes Projets</title>
+</head>
+<body>
+<header>
+        <?php include 'header.php'; ?>
+</header>
+
+<div class="projet1">
+    <h1>Site ecom</h1>
+</div>
+
+
+
+
+
+
+
+<footer>
+<?php include 'footer.php'; ?>
+</footer>
+</body>
+</html>
